ea87e7d92ddda89bead67f37cd7950868b3b8086
Zac Gladman <zacharygladman@yahoo.co.uk>
2022-11-17 14:32:14 +0000

    test CountrySpecificView with Brazil

--- src/components/CountrySpecificView.tsx
+++ src/components/CountrySpecificView.tsx
@@ -2,23 +2,25 @@ import { useEffect, useState } from "react";
 import { Country } from "../CountryInterface";
 
 
-const randomCountry = () => {
-    const countries = ['United States', 'Brazil', 'Belgium', 'Norway', 'Guatemala', 'South Africa'];
-    const randomIndex = Math.floor(Math.random()*(countries.length-1))
-    return countries[randomIndex]
-}
+// const randomCountry = () => {
+//     const countries = ['United States', 'Brazil', 'Belgium', 'Norway', 'Guatemala', 'South Africa'];
+//     const randomIndex = Math.floor(Math.random()*(countries.length-1))
+//     return countries[randomIndex]
+// }
 
 export default function CountrySpecificView(): JSX.Element {
 
     const [country, setCountry] = useState<Country>()
-    const randCountry = randomCountry()
+    
     useEffect(() => {
         const fetchCountry = async () => {
+            // const randCountry = randomCountry()
             const reponse = await fetch (
-                `https://restcountries.com/v3.1/name/${randCountry}?fullText=true`
+                `https://restcountries.com/v3.1/name/Brazil?fullText=true`
             )
             const jsonBody: Country[] = await reponse.json()
             setCountry(jsonBody[0])
+            console.log(jsonBody[0])
         }
         fetchCountry()
     }, [])
